753e4d9e78a99127ab2fbea693f50ed7bc63bfd3
Krishna Alex <krishna.aromal@gmail.com>
2023-05-26 06:38:10 -0700

    create and save the model for a new item.

--- ToDoList/ToDoDetailTableViewController.swift
+++ ToDoList/ToDoDetailTableViewController.swift
@@ -15,12 +15,34 @@ class ToDoDetailTableViewController: UITableViewController {
     @IBOutlet weak var notesTextView: UITextView!
     @IBOutlet weak var saveButton: UIBarButtonItem!
     
+    var isDatePickerHidden = true
+    let dateLabelIndexPath = IndexPath(row: 0, section: 1)
+    let datePickerIndexPath = IndexPath(row: 1, section: 1)
+    let notesIndexPath = IndexPath(row: 0, section: 2)
+    
+    var toDo : ToDo?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        dueDateDatePicker.date = Date().addingTimeInterval(24*60*60)
         updateSaveButtonState()
         updateDueDateLabel(date: dueDateDatePicker.date)
+        
        
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        super.prepare(for: segue, sender: sender)
+        
+        guard segue.identifier == "saveUnwind" else { return }
+        
+        let title = titleTextField.text!
+        let isComplete = isCompleteButton.isSelected
+        let dueDate = dueDateDatePicker.date
+        let notes = notesTextView.text
+        
+        toDo = ToDo(title: title, isComplete: isComplete, dueDate: dueDate, notes: notes)
     }
     
     func updateSaveButtonState() {
@@ -50,5 +72,38 @@ class ToDoDetailTableViewController: UITableViewController {
         updateDueDateLabel(date: sender.date)
     }
     
+    override func tableView(_ tableView: UITableView,
+       heightForRowAt indexPath: IndexPath) -> CGFloat {
+        switch indexPath {
+        case datePickerIndexPath where isDatePickerHidden == true:
+            return 0
+        case notesIndexPath:
+            return 200
+        default:
+            return UITableView.automaticDimension
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
+        switch indexPath {
+        case datePickerIndexPath:
+            return 216
+        case notesIndexPath:
+            return 200
+        default:
+            return UITableView.automaticDimension
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        if indexPath == dateLabelIndexPath {
+            isDatePickerHidden.toggle()
+            updateDueDateLabel(date: dueDateDatePicker.date)
+            tableView.beginUpdates()
+            tableView.endUpdates()
+        }
+    }
+   
     
 }
